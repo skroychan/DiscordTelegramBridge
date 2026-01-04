c8d4098c92f0c261ebfc15a7d2b8eafbdf157378
skroy <skroychan@gmail.com>
2026-01-05 00:32:58 +0400

    wip embed support

--- main.py
+++ main.py
@@ -22,10 +22,6 @@ async def send_to_discord(username, text, attachment_url=None, has_spoiler=False
     if channel:
         result = f"**{username}**: {text}"
 
-        if attachment_url:    
-            data = await download_file(attachment_url)
-            file = discord.File(data, attachment_url.split("/")[-1], spoiler=has_spoiler)
-
         if quote_text:
             quote_text = quote_text.replace("\n", "\n> ")
             if quote_username:
@@ -33,13 +29,19 @@ async def send_to_discord(username, text, attachment_url=None, has_spoiler=False
             else:
                 result = f"> {quote_text}\n{result}"
 
-        await channel.send(result, file=file)
+        if attachment_url:    
+            data = await download_file(attachment_url)
+            file = discord.File(data, attachment_url.split("/")[-1], spoiler=has_spoiler)
+            await channel.send(result, file=file)
+
+        else:
+            await channel.send(result)
 
 
 def escape_markdown(str):
     return re.sub(r"([_\*\[\]\(\)~`>#+\-=|\{\}\.!])", r"\\\1", str)
 
-async def send_to_telegram(username, text, attachments=None, quote_username=None, quote_text=None):
+async def send_to_telegram(username, text, attachments=[], embeds=[], quote_username=None, quote_text=None):
     result = f"*{escape_markdown(username)}*: {escape_markdown(text)}"
 
     if quote_text:
@@ -50,8 +52,8 @@ async def send_to_telegram(username, text, attachments=None, quote_username=None
         else:
             result = f">{quote_text}\n{result}"
 
-    if attachments:
-        if len(attachments) > 1:
+    if attachments or embeds:
+        if len(attachments) + len(embeds) > 1:
             media_group = []
             for attachment in attachments:
                 if attachment.content_type.startswith("image"):
@@ -62,21 +64,36 @@ async def send_to_telegram(username, text, attachments=None, quote_username=None
                     media_group.append(InputMediaPhoto(attachment.url, show_caption_above_media=True))
                 else:
                     media_group.append(InputMediaDocument(attachment.url, show_caption_above_media=True))
+            
+            for embed in embeds:
+                result = result.replace(escape_markdown(embed.url), "", 1)
+                if embed.type == "image":
+                    media_group.append(InputMediaPhoto(embed.image.proxy_url or embed.image.url or embed.url, show_caption_above_media=True))
+                elif embed.type == "video" or embed.type == "gifv":
+                    media_group.append(InputMediaVideo(embed.video.proxy_url or embed.video.url or embed.url, show_caption_above_media=True))
                     
             media_group[0].caption = result
             
             await telegram_bot.send_media_group(telegram_chat_id, media_group, message_thread_id=telegram_message_thread_id)
 
         else:
-            attachment = attachments[0]
-            if attachment.content_type.startswith("image"):
-                await telegram_bot.send_photo(telegram_chat_id, attachment.url, result, show_caption_above_media=True, has_spoiler=attachment.is_spoiler(), message_thread_id=telegram_message_thread_id)
-            elif attachment.content_type.startswith("video"):
-                await telegram_bot.send_video(telegram_chat_id, attachment.url, caption=result, show_caption_above_media=True, has_spoiler=attachment.is_spoiler(), message_thread_id=telegram_message_thread_id)
-            elif attachment.content_type.startswith("audio"):
-                await telegram_bot.send_audio(telegram_chat_id, attachment.url, result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
-            else:
-                await telegram_bot.send_document(telegram_chat_id, attachment.url, caption=result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
+            if attachments:
+                attachment = attachments[0]
+                if attachment.content_type.startswith("image"):
+                    await telegram_bot.send_photo(telegram_chat_id, attachment.url, result, show_caption_above_media=True, has_spoiler=attachment.is_spoiler(), message_thread_id=telegram_message_thread_id)
+                elif attachment.content_type.startswith("video"):
+                    await telegram_bot.send_video(telegram_chat_id, attachment.url, caption=result, show_caption_above_media=True, has_spoiler=attachment.is_spoiler(), message_thread_id=telegram_message_thread_id)
+                elif attachment.content_type.startswith("audio"):
+                    await telegram_bot.send_audio(telegram_chat_id, attachment.url, result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
+                else:
+                    await telegram_bot.send_document(telegram_chat_id, attachment.url, caption=result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
+            if embeds:
+                embed = embeds[0]
+                result = result.replace(escape_markdown(embed.url), "", 1)
+                if embed.type == "image":
+                    await telegram_bot.send_photo(telegram_chat_id, embed.image.proxy_url or embed.image.url or embed.url, result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
+                elif embed.type == "video" or embed.type == "gifv":
+                    await telegram_bot.send_video(telegram_chat_id, embed.video.proxy_url or embed.video.url or embed.url, caption=result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
 
     else:
         await telegram_bot.send_message(telegram_chat_id, result, message_thread_id=telegram_message_thread_id)
@@ -87,21 +104,21 @@ class DiscordBot(discord.Client):
         if message.author == self.user or message.channel.id != discord_chat_id:
             return
 
-        text = message.clean_content
+        text = message.system_content if message.is_system() else message.clean_content
         username = message.author.global_name or message.author.name
 
         if message.type == discord.MessageType.reply:
             replied_to = await message.channel.fetch_message(message.reference.message_id)
 
-            quote = replied_to.clean_content
+            quote = replied_to.system_content if replied_to.is_system() else replied_to.clean_content
             quote_author = None
             if replied_to.author != self.user:
                 quote_author = replied_to.author.global_name or replied_to.author.name
 
-            await send_to_telegram(username, text, message.attachments, quote_author, quote)
+            await send_to_telegram(username, text, message.attachments, message.embeds, quote_author, quote)
 
         else:
-            await send_to_telegram(username, text, message.attachments)
+            await send_to_telegram(username, text, message.attachments, message.embeds)
 
     async def setup_hook(self):
         self.bg_task = self.loop.create_task(self.telegram_task())
