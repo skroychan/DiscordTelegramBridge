48e6f920227d934f5035679e4a201a12e65c2862
skroy <skroychan@gmail.com>
2026-01-05 16:22:26 +0400

    improve attachment and embed support

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import re
 import io, aiohttp
+import os
+from urllib.parse import urlparse, unquote
 
 import discord
 from telebot.async_telebot import AsyncTeleBot
@@ -10,6 +12,13 @@ from config import discord_token, discord_chat_id, telegram_token, telegram_chat
 
 telegram_bot = AsyncTeleBot(telegram_token, parse_mode="MarkdownV2")
 
+
+def get_filename_from_url(url):
+    parsed_url = urlparse(url)
+    return os.path.basename(unquote(parsed_url.path))
+
+def get_extension(filename):
+    return os.path.splitext(filename)[1]
 
 async def download_file(url):
     async with aiohttp.ClientSession() as session:
@@ -29,9 +38,9 @@ async def send_to_discord(username, text, attachment_url=None, has_spoiler=False
             else:
                 result = f"> {quote_text}\n{result}"
 
-        if attachment_url:    
+        if attachment_url:
             data = await download_file(attachment_url)
-            file = discord.File(data, attachment_url.split("/")[-1], spoiler=has_spoiler)
+            file = discord.File(data, get_filename_from_url(attachment_url), spoiler=has_spoiler)
             await channel.send(result, file=file)
 
         else:
@@ -64,36 +73,43 @@ async def send_to_telegram(username, text, attachments=[], embeds=[], quote_user
                     media_group.append(InputMediaPhoto(attachment.url, show_caption_above_media=True))
                 else:
                     media_group.append(InputMediaDocument(attachment.url, show_caption_above_media=True))
-            
+
             for embed in embeds:
                 result = result.replace(escape_markdown(embed.url), "", 1)
                 if embed.type == "image":
                     media_group.append(InputMediaPhoto(embed.image.proxy_url or embed.image.url or embed.url, show_caption_above_media=True))
                 elif embed.type == "video" or embed.type == "gifv":
                     media_group.append(InputMediaVideo(embed.video.proxy_url or embed.video.url or embed.url, show_caption_above_media=True))
-                    
+
             media_group[0].caption = result
-            
+
             await telegram_bot.send_media_group(telegram_chat_id, media_group, message_thread_id=telegram_message_thread_id)
 
         else:
             if attachments:
                 attachment = attachments[0]
                 if attachment.content_type.startswith("image"):
-                    await telegram_bot.send_photo(telegram_chat_id, attachment.url, result, show_caption_above_media=True, has_spoiler=attachment.is_spoiler(), message_thread_id=telegram_message_thread_id)
+                    if get_extension(get_filename_from_url(attachment.url)) == ".gif":
+                        await telegram_bot.send_animation(telegram_chat_id, attachment.url, caption=result, show_caption_above_media=True, has_spoiler=attachment.is_spoiler(), message_thread_id=telegram_message_thread_id)
+                    else:
+                        await telegram_bot.send_photo(telegram_chat_id, attachment.url, result, show_caption_above_media=True, has_spoiler=attachment.is_spoiler(), message_thread_id=telegram_message_thread_id)
                 elif attachment.content_type.startswith("video"):
                     await telegram_bot.send_video(telegram_chat_id, attachment.url, caption=result, show_caption_above_media=True, has_spoiler=attachment.is_spoiler(), message_thread_id=telegram_message_thread_id)
                 elif attachment.content_type.startswith("audio"):
                     await telegram_bot.send_audio(telegram_chat_id, attachment.url, result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
                 else:
                     await telegram_bot.send_document(telegram_chat_id, attachment.url, caption=result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
+
             if embeds:
                 embed = embeds[0]
                 result = result.replace(escape_markdown(embed.url), "", 1)
-                if embed.type == "image":
-                    await telegram_bot.send_photo(telegram_chat_id, embed.image.proxy_url or embed.image.url or embed.url, result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
-                elif embed.type == "video" or embed.type == "gifv":
+                if embed.type == "video" or embed.type == "gifv":
                     await telegram_bot.send_video(telegram_chat_id, embed.video.proxy_url or embed.video.url or embed.url, caption=result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
+                elif embed.type == "image":
+                    if get_extension(get_filename_from_url(embed.image.proxy_url or embed.image.url or embed.url)) == ".gif":
+                        await telegram_bot.send_animation(telegram_chat_id, embed.video.proxy_url or embed.video.url or embed.url, caption=result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
+                    else:
+                        await telegram_bot.send_photo(telegram_chat_id, embed.image.proxy_url or embed.image.url or embed.url, result, show_caption_above_media=True, message_thread_id=telegram_message_thread_id)
 
     else:
         await telegram_bot.send_message(telegram_chat_id, result, message_thread_id=telegram_message_thread_id)
@@ -105,7 +121,7 @@ class DiscordBot(discord.Client):
             return
 
         text = message.system_content if message.is_system() else message.clean_content
-        username = message.author.global_name or message.author.name
+        username = message.author.display_name or message.author.global_name or message.author.name
 
         if message.type == discord.MessageType.reply:
             replied_to = await message.channel.fetch_message(message.reference.message_id)
